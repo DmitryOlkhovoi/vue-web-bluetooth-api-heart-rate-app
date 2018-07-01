dc96b91322cfd36f08f958e405a1fa98d1a8183f
Dmitry Olkhovoi <auuine@gmail.com>
2018-07-01 20:54:29 +0300

    Add GH page

---
+++ docs/precache-manifest.8101f18c773c4b0418d7a2d6312ebe89.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d34b1037ff5ac2bb91fb",
+    "url": "/js/chunk-vendors.d34b1037.js"
+  },
+  {
+    "revision": "a80ffd5da696651d0639",
+    "url": "/js/app.a80ffd5d.js"
+  },
+  {
+    "revision": "6a100767cb9dc5704ff4121a2a97869d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a80ffd5da696651d0639",
+    "url": "/css/app.04ad18c4.css"
+  }
+];
